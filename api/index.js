40fec8456f541d5d82a4fdd72a6cdcdf24a6bf3e
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-09-12 18:14:39 -0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,32 @@
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const server = require('./src/app.js');
+const allGenres = require('./src/controllers/getGenresController.js');
+const { conn } = require('./src/db.js');
+require('dotenv').config();
+
+// Syncing all the models at once.
+//! una vez terminado el desarrollo cambiar a {force: false}
+conn.sync({ force: false }).then(() => {
+  server.listen(process.env.PORT, () => {
+    allGenres();
+    console.log('%s listening', process.env.PORT); // eslint-disable-line no-console
+  });
+});
