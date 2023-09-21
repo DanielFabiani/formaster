a188f0f00e72a847067cda140ad0a20ac3d2f78a
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-09-21 19:14:50 -0300

    update port back

--- api/index.js
+++ api/index.js
@@ -20,11 +20,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
+const port = process.env.PORT ?? 8080;
 
 // Syncing all the models at once.
 //! una vez terminado el desarrollo cambiar a {force: false}
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening', process.env.PORT); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log('%s listening', port); // eslint-disable-line no-console
   });
 });
