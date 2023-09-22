609199bdb08c121b7294d9669085822b95d60c9f
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-09-21 22:15:43 -0300

    update port back 5

--- api/src/db.js
+++ api/src/db.js
@@ -2,10 +2,9 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DATABASE_URL} = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+const sequelize = new Sequelize(DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -13,9 +12,9 @@ const sequelize = new Sequelize(
 );
 
 //test de conexión 
-sequelize.authenticate()
+/* sequelize.authenticate()
   .then(() => console.log('Connection has been established successfully.'))
-  .catch(error => console.log('Unable to connect to the database:', error))
+  .catch(error => console.log('Unable to connect to the database:', error)) */
 
 const basename = path.basename(__filename);
 
