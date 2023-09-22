1089a0ea9aaf9ce7a8389e86c9234cbbef99f076
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-09-21 23:05:15 -0300

    back y db con render 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
-import axios from "axios";
 import AnswersForm from "./components/pages/AnswersForm/AnswersForm";
 import UpdateAnswers from "./components/pages/UpdateAnswers/UpdateAnswers";
+import axios from "axios";
 //axios.defaults.baseURL = 'http://localhost:3001/';
-axios.defaults.baseURL = 'https://formaster.onrender.com/';
+axios.defaults.baseURL = 'https://formaster.onrender.com';
 
 function App() {
   return (
