b50280ac3e600a0975866964b9c2932967f1a145
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-09-22 21:05:21 -0300

    prueba deploy 8

--- api/src/controllers/getAnswersFormController.js
+++ api/src/controllers/getAnswersFormController.js
@@ -6,12 +6,17 @@ const { Form } = require("../db");
 const answersFormData = async () => {
 
   const lastForm = await Form.findOne({
-    order: [[ 'id', 'ASC']] // Ordena por id en orden descendente
+    order: [['id', 'DESC']] // Ordena por id en orden descendente
   });
-
+  
+  let nextId;
+  
   if (!lastForm) {
-    console.log('No se encontró el formulario');
-    return null;
+    // Si no se encontró ningún formulario, asigna el ID 1 como el siguiente
+    nextId = 1;
+  } else {
+    // Calcula el siguiente ID como el último ID + 1
+    nextId = lastForm.id + 1;
   }
 
   /* const lastId = await Form.max('id');
